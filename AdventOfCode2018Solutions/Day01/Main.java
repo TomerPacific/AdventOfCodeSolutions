b860f1efafabe6f9eca315d30c912c18a6422808
Tomer Ben Rachel <tomerpacific@gmail.com>
2018-12-07 09:01:37 +0200

    AdventOfCode2018Solutions Day01 Second half solution

--- AdventOfCode2018Solutions/Day01/Main.java
+++ AdventOfCode2018Solutions/Day01/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -8,27 +9,41 @@ public class Main {
 
         String line;
         Integer sum = 0;
-        try
-        {
-            BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
+        Boolean notFound = false;
+        ArrayList<Integer> frequencies = new ArrayList<Integer>();
 
-            while ((line = reader.readLine()) != null)
+        while (!notFound) {
+            try
             {
-               Character sign = line.charAt(0);
-               if (sign == '+') {
-                   sum += Integer.parseInt(line.substring(1));
-               } else {
-                   sum -= Integer.parseInt(line.substring(1));
-               }
+                BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
 
+                while ((line = reader.readLine()) != null)
+                {
+                    Character sign = line.charAt(0);
+                    if (sign == '+') {
+                        sum += Integer.parseInt(line.substring(1));
+                    } else {
+                        sum -= Integer.parseInt(line.substring(1));
+                    }
+
+                    if (frequencies.contains(sum)) {
+                        System.out.println("Already seen frequency " + sum);
+                        notFound = true;
+                        break;
+                    } else {
+                        frequencies.add(sum);
+                    }
+
+                }
+                reader.close();
+                System.out.println("Resulting frequency is " + sum);
             }
-            reader.close();
-            System.out.println("Resulting frequency is " + sum);
+            catch (Exception e)
+            {
+                System.err.format("Exception occurred trying to read '%s'.", FILENAME);
+                e.printStackTrace();
+            }
         }
-        catch (Exception e)
-        {
-            System.err.format("Exception occurred trying to read '%s'.", FILENAME);
-            e.printStackTrace();
-        }
+
     }
 }
