41a711019b4e468376d1fcbb80cda6af739895bd
unknown <tomerpacific@gmail.com>
2019-12-03 19:32:54 +0200

    AdventOfCodeSolutions2019 Day01 Second half

--- AdventOfCode2019Solutions/Day01/Main.java
+++ AdventOfCode2019Solutions/Day01/Main.java
@@ -15,8 +15,8 @@ public class Main {
             while (myReader.hasNextLine()) {
               String data = myReader.nextLine();
               Double mass = Double.valueOf(data);
-              mass = Math.floor(mass / 3) - 2;
-              overallFuelRequirement += mass;
+              Double fuelForMass = calculateOverallFuelForMass(mass);
+              overallFuelRequirement += fuelForMass;
             }
             System.out.println("Overall " + overallFuelRequirement);
             myReader.close();
@@ -26,4 +26,21 @@ public class Main {
           }
 
     }
+
+  private static Double calculateOverallFuelForMass(Double mass) {
+    
+    Double result = 0.0;
+    Double fuel;
+    while(mass > 0) {
+      fuel = Math.floor(mass / 3) - 2;
+      if (fuel > 0) {
+        result += fuel;
+      }
+      
+      mass = fuel;
+    }
+   
+    return result;
+  }
+
 }
